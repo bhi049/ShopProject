c3820b102eb85c9a989da9a5b41231c8bcc949a2
bhi049 <bhi049@myy.haaga-helia.fi>
2025-05-16 16:17:10 +0300

    Update product method

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
 
@@ -8,6 +9,17 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); // allows us to parse JSON data from the request body
+
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+})
 
 app.post("/api/products", async (req, res) => {
   const product = req.body; // user will send this data
@@ -22,11 +34,28 @@ app.post("/api/products", async (req, res) => {
     await newProduct.save();
     res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
-    console.error("Error creating product:", error);
+    console.error("Error creating product:", error.message);
     res.status(500).json({ success: false, message: "Server Errror"})
   }
 
  });
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid product ID" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.error("Error updating product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
@@ -35,7 +64,7 @@ app.delete("/api/products/:id", async (req, res) => {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted successfully" });
   } catch (error) {
-    console.error("Error deleting product:", error);
+    console.error("Error deleting product:", error.message);
     res.status(404).json({ success: false, message: "Product not found" });
   }
 });

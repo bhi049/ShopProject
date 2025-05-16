696509e04e5dde35f69b7d827136362fcd4d1103
bhi049 <bhi049@myy.haaga-helia.fi>
2025-05-16 14:53:58 +0300

    Connect to database, product model created with post and delete endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,46 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // allows us to parse JSON data from the request body
 
-app.get("api/products", (req, res) => {
-  res.send('Server is running');
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: "Please fill all the fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    res.status(500).json({ success: false, message: "Server Errror"})
+  }
+
+ });
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
 });
 
 app.listen(5001, () => {
+  connectDB();
   console.log('Server is running at http://localhost:5001');
 });

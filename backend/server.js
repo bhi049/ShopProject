e108234f592b6004d5acfb05d0644c85f846a5e6
bhi049 <bhi049@myy.haaga-helia.fi>
2025-05-17 18:09:57 +0300

    Freontend started

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // allows us to parse JSON data from the request body
 
 app.use("/api/products", productRoutes); // all routes starting with /api/products will be handled by productRoutes
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server is running at http://localhost:5001');
+  console.log('Server is running at http://localhost:' + PORT);
 });
---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+
+// optional, you can define your own theme later
+const theme = extendTheme({})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+)
+
